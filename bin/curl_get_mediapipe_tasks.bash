390dc76145c8631a84b51e07150c815d1725e51f
PapaKumo <jdbar1215@gmail.com>
2024-03-03 19:39:40 -0800

    Refactor

--- bin/curl_get_mediapipe_tasks.bash
+++ bin/curl_get_mediapipe_tasks.bash
@@ -6,30 +6,23 @@ mkdir -p public/assets
 
 downloaded_files=0
 
+# Function to download a task file if it doesn't exist
+download_task_file() {
+	local task_file_name=$1
+	local task_file_url=$2
+
+	if [ ! -f "public/assets/$task_file_name" ]; then
+		echo "Downloading $task_file_name to public/assets/"
+		curl -# -o "public/assets/$task_file_name" "$task_file_url"
+		downloaded_files=$((downloaded_files+1))
+	fi
+}
+
 # Download the .task files
-if [ ! -f public/assets/pose_landmarker_lite.task ]; then
-	echo "Downloading pose_landmarker_lite.task to public/assets/"
-	curl -# -o public/assets/pose_landmarker_lite.task https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/latest/pose_landmarker_lite.task
-	downloaded_files=$((downloaded_files+1))
-fi
-
-if [ ! -f public/assets/pose_landmarker_full.task ]; then
-	echo "Downloading pose_landmarker_full.task to public/assets/"
-	curl -# -o public/assets/pose_landmarker_full.task https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_full/float16/latest/pose_landmarker_full.task
-	downloaded_files=$((downloaded_files+1))
-fi
-
-if [ ! -f public/assets/pose_landmarker_heavy.task ]; then
-	echo "Downloading pose_landmarker_heavy.task to public/assets/"
-	curl -# -o public/assets/pose_landmarker_heavy.task https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_heavy/float16/latest/pose_landmarker_heavy.task
-	downloaded_files=$((downloaded_files+1))
-fi
-
-if [ ! -f public/assets/face_landmarker.task ]; then
-	echo "Downloading face_landmarker.task to public/assets/"
-	curl -# -o public/assets/face_landmarker.task https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/latest/face_landmarker.task
-	downloaded_files=$((downloaded_files+1))
-fi
+download_task_file "pose_landmarker_lite.task" "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/latest/pose_landmarker_lite.task"
+download_task_file "pose_landmarker_full.task" "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_full/float16/latest/pose_landmarker_full.task"
+download_task_file "pose_landmarker_heavy.task" "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_heavy/float16/latest/pose_landmarker_heavy.task"
+download_task_file "face_landmarker.task" "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/latest/face_landmarker.task"
 
 # Print done if files were downloaded
 if [ $downloaded_files -gt 0 ]; then
